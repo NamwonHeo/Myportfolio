a60745dedc9b5d6d2e7dc09600096617f599edb0
Namwon Heo <namwonheo89@gmail.com>
2017-11-01 23:44:40 +0900

    first commit

---
+++ app/controllers/mainview_controller.rb
@@ -0,0 +1,2 @@
+class MainviewController < ApplicationController
+end
---
+++ test/controllers/mainview_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MainviewControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

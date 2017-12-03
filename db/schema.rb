e1e86d3887b9bc545eae6c767b020275180702a2
Namwon Heo <namwonheo89@gmail.com>
2017-12-03 23:02:09 +0900

    postgresql installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118170412) do
+ActiveRecord::Schema.define(version: 20171112081439) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -22,13 +22,6 @@ ActiveRecord::Schema.define(version: 20171118170412) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.index ["type"], name: "index_ckeditor_assets_on_type"
-  end
-
-  create_table "designs", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "specs", force: :cascade do |t|
